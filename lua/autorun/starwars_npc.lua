e66a48a77fcfc0288ec9f9994c872bcdf21761f2
Zerogravitygamerz <77190934+Zerogravitygamerz@users.noreply.github.com>
2025-07-27 09:01:55 +0100

    Improve frozen NPC script

--- lua/autorun/starwars_npc.lua
+++ lua/autorun/starwars_npc.lua
@@ -2,17 +2,22 @@
 
 if SERVER then
     hook.Add("InitPostEntity", "SpawnFrozenStarWarsNPC", function()
+        -- Position and orientation for the NPC
         local pos = Vector(-10322.2559, -848.5197, -6086.9688)
+        local ang = Angle(0, 0, 0)
 
         local npc = ents.Create("npc_citizen")
         if not IsValid(npc) then return end
 
         npc:SetModel("models/aussiwozzi/cgi/base/212th_arc.mdl")
         npc:SetPos(pos)
+        npc:SetAngles(ang)
         npc:Spawn()
 
-        -- Prevent the NPC from moving
+        -- Keep the NPC completely frozen
         npc:CapabilitiesClear()
+        npc:SetNPCState(NPC_STATE_SCRIPT)
+        npc:SetSchedule(SCHED_IDLE_STAND)
         npc:SetMoveType(MOVETYPE_NONE)
     end)
 end
